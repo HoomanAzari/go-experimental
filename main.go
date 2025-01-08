d42c74863e4fc31f0eda2922032b617991b11c3e
Hooman Azari <veryhouman@gmail.com>
2025-01-08 01:30:26 -0500

    added new routes

--- main.go
+++ main.go
@@ -7,9 +7,19 @@ import (
 )
 
 func main() {
-	// Handler function
+	// Root Handler function
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world!")
+		fmt.Fprintf(w, "Welcome to the homepage!")
+	})
+
+	// About Hnadler function
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This is the About page!")
+	})
+
+	// Contact Handler function
+	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Contact me at veryhouman@gmail.com")
 	})
 
 	// Start the server on port 8080
